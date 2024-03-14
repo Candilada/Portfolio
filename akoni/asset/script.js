bd3659b6de808818e3b8c24a1f81f6ecb1b10348
Candilada <152843047+Candilada@users.noreply.github.com>
2024-03-14 16:23:39 +0800

    Delete akoni directory

--- akoni/asset/script.js
+++
@@ -1,6 +0,0 @@
-function navigateTo(page) {
-    document.querySelector('main').classList.add('slide-left');
-    setTimeout(function() {
-        window.location.href = page;
-    }, 300); // Adjust the timing based on your transition duration
-}
